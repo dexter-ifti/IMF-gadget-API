ee09eb656705cce8efd14d102aa5c3d5f83dd208
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-02-08 01:18:14 +0530

    Update Swagger routes to point to index.js for improved routing structure

--- swagger.js
+++ swagger.js
@@ -9,7 +9,7 @@ const doc = {
 };
 
 const outputFile = './swagger-output.json';
-const routes = ['./path/userRoutes.js', './path/bookRoutes.js'];
+const routes = ['./index.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
